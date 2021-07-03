3295202bbf002d80671926c2582435be2415dbf9
Quy Le Anh <quyleanh121093@gmail.com>
2021-07-04 01:10:39 +0700

    update

---
+++ portfolio/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for file in `find . -name "*.jpg"`; do
+    npx @squoosh/cli --mozjpeg '{"quality":95,"baseline":false,"arithmetic":false,"progressive":true,"optimize_coding":true,"smoothing":0,"color_space":3,"quant_table":3,"trellis_multipass":false,"trellis_opt_zero":false,"trellis_opt_table":false,"trellis_loops":1,"auto_subsample":true,"chroma_subsample":2,"separate_chroma_quality":false,"chroma_quality":75}' $file
+done
